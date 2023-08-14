3308cd896cbae0f504181ea73b77967bd6329780
Umi252 <umisiguca25@gmail.com>
2023-08-14 11:11:20 +0000

    python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+# 7-add_tuple.py
+
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Fill missing elements with zeros if needed
-    padded_tuple_a = tuple_a + (0, 0)
-    padded_tuple_b = tuple_b + (0, 0)
-    
-    # Add corresponding elements and create the result tuple
-    result_tuple = (padded_tuple_a[0] + padded_tuple_b[0],
-                    padded_tuple_a[1] + padded_tuple_b[1])
-    
-    return result_tuple
+    """Add two tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
