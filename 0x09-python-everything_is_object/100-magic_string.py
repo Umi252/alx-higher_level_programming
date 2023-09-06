8bdd086ca8fd264fb7e31c825ec2e9f7703f4c7d
Umi252 <umisiguca25@gmail..com>
2023-09-06 12:19:01 -0700

    obeject turns to magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n=1):
-    n = max(n, 1)
-    return ", ".join(["BestSchool"] * n)
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
