71c176b4dd94e3691d12c82b75392cfe1e64644b
Umi252 <umisiguca25@gmail..com>
2023-12-01 00:37:15 -0800

    for the test

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Please provide a URL as an argument."
-    exit 1
-fi
-
-URL=$1
-RESPONSE_SIZE=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
-echo "Response body size in bytes: $RESPONSE_SIZE"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script to display the body of a 200 status code response
-
-[ -z "$1" ] && echo "Please provide a URL as an argument." && exit 1
-curl -s -o response.txt -w "%{http_code}" "$1" --output - | awk '/^200$/{body=1;next} body{print}'
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
